6093f706097649d1e96cfeeba14654ea5cbfd8ae
3116005181 <1470064691@qq.com>
2018-09-12 23:15:19 +0800

    -c / -w / -l / -s 命令完成，输入参数改善

---
+++ wc.js
@@ -0,0 +1,18 @@
+const order = require('./lib/order')
+const FileData = require('./lib/filedata')
+// FileData.readUrl(__dirname, { outOrder: '-c', readOrder: '-s' })
+// FileData.readUrl('./main.js', { outOrder: ['-c', '-w'], readOrder: [] })
+// FileData.readUrl(__dirname, { outOrder: ['-c', '-w'], readOrder: [] })
+
+order(function(url, input, output){
+  FileData.readUrl(url, { outOrder: output, readOrder: input })
+})
+
+/**
+ * -c file.c     //返回文件 file.c 的字符数
+ * -w file.c    //返回文件 file.c 的词的数目  
+ * -l file.c      //返回文件 file.c 的行数
+ * -s   递归处理目录下符合条件的文件。
+ * -a   返回更复杂的数据（代码行 / 空行 / 注释行）。
+ * [file_name]: 文件或目录名，可以处理一般通配符
+ */
